ccc3ffeef88f99535ae4c4505c22252aa0a47b5a
Rajan Kumar <rajan.kuma.rk.0502@gmail.com>
2023-12-07 18:02:13 +0530

    questions on basic hashing

---
+++ Basic_Hashing/CountNumberFrequency.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountNumberFrequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        System.out.println(map);
+
+        sc.close();
+    }
+}
